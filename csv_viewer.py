17684378c3ec7f76f60fac8de3b4a523298dc8f3
Michael Pagitsch <michael.pagitsch@rwth-aachen.de>
2019-11-18 16:45:31 +0100

    Anzeige der Eigenschaften der geladenen Datei
    Name, Größe, Spalten- und Zeilenanzahl der geladenen Datei werden
    angezeigt

--- csv_viewer.py
+++ csv_viewer.py
@@ -37,8 +37,16 @@ def load_data():
             # Set dropdown menu
             options = list(data.columns)
             lst_dataselection.config(values=options)
+            
+            # Set fileinfo
+            filesize_MB = os.path.getsize(file_path)/1048576
+            filelength = len(data)
+            number_of_cols = len(data.columns)
+            file_name = os.path.split(file_path)[1]
+            status_fileinfo.set('{} ({:.1f} MB / {} lines / {} columns)'.format(file_name, filesize_MB, filelength, number_of_cols))
         except:
             status.set('ERROR: Could not read file <{}>.'.format(file_path))
+            reset_fileinfo()
     else:  # Datei nicht vorhanden
         status.set('ERROR: File <{}> does not exist!'.format(file_path))
         #lbl_status.config(fg='red')  # Nur zum Test: Schriftfarbe ändern
@@ -72,6 +80,9 @@ def reset_plot():
 def reset_dropdown():
     lst_dataselection.config(values=[])
 
+def reset_fileinfo():
+    status_fileinfo.set('No file loaded.')
+
 
 ###  GUI-Elemente definieren  #################################################
 
@@ -79,6 +90,7 @@ window = tk.Tk()
 window.geometry(newGeometry='1365x780+0+0')
 window.title('CSV-Viewer')
 window.resizable(0, 0)
+window.iconbitmap('./resources/csv.ico')
 
 # Variablen
 radio_sep = tk.StringVar(window)
@@ -88,39 +100,40 @@ radio_decimal.set('.')  # Default value
 plotselection = tk.StringVar(window)
 options = []
 frm_plot = tk.Frame(master=window)
-
-
-fig = plt.Figure(figsize=(13, 6), dpi=100)
-t = np.arange(0, 3, .01)
-fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 status = tk.StringVar(window)
+status_fileinfo = tk.StringVar(window)
+reset_fileinfo()
+
+# Dateneingabe
 frm_input = tk.Frame(master=window)
 lbl_path = tk.Label(master=frm_input, text='Full path to data file:')
 input_path = tk.Entry(master=frm_input, bd=1, width=70)
 lbl_sep = tk.Label(master=frm_input, text='Column separator:')
 lbl_dec = tk.Label(master=frm_input, text='Decimal:')
-
-
 radio_sep1 = tk.Radiobutton(master=frm_input, variable=radio_sep, text=',', value=',')
 radio_sep2 = tk.Radiobutton(master=frm_input, variable=radio_sep, text=';', value=';')
 radio_sep3 = tk.Radiobutton(master=frm_input, variable=radio_sep, text='tab', value=r'\t')
-
 radio_decimal1 = tk.Radiobutton(master=frm_input, variable=radio_decimal, text='.', value='.')
 radio_decimal2 = tk.Radiobutton(master=frm_input, variable=radio_decimal, text=',', value=',')
 btn_load_data = tk.Button(master=frm_input, text='Load data', command=load_data)
+
+# Datenauswahl
 frm_dataselection = tk.Frame(master=window)
-
-
 lbl_dataselection = tk.Label(master=frm_dataselection, text='Select plot data:')
 lst_dataselection = ttk.Combobox(master=frm_dataselection, values=options)
 btn_dataselection = tk.Button(master=frm_dataselection, text='Update plot', command=update_plot)
 
+# Plot
+fig = plt.Figure(figsize=(13, 6), dpi=100)
+t = np.arange(0, 3, .01)
+fig.add_subplot(111).plot(t, 2 * np.sin(2 * np.pi * t))
 canvas = tkagg.FigureCanvasTkAgg(fig, master=window)
 canvas.draw()
 
+# Status
 frm_status = tk.Frame(master=window, bg='#808080')
-
 lbl_status = tk.Label(master=frm_status, textvariable=status, bg='#808080', fg='#ffffff')
+lbl_status_fileinfo = tk.Label(master=frm_status, textvariable=status_fileinfo,  bg='#808080', fg='#ffffff')
 
 
 ###  GUI-Elemente platzieren  #################################################
@@ -152,6 +165,7 @@ tkagg.NavigationToolbar2Tk(canvas, frm_plot)
 # Status
 frm_status.pack(side='bottom', padx='7', pady='7', fill='x', expand=True)
 lbl_status.pack(side='left', padx='7')
+lbl_status_fileinfo.pack(side='right', padx='7')
 
 set_status()
 reset_plot()
